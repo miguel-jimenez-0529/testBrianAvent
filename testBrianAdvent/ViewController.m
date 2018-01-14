8e4c3de99c159aedb9eda6bc20ce57448857c1a5
Miguel Jimenez <migueljimenez@imac-de-maria.local>
2018-01-14 15:37:58 -0600

    Added some variables

--- testBrianAdvent/ViewController.m
+++ testBrianAdvent/ViewController.m
@@ -17,6 +17,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSString *hello = @"Hello";
+    NSInteger num  = 4;
+    NSLog(hello);
+    NSLog(@"%@", num);
 }
 
 
